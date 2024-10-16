6167003ec2d4caee0079372809443890502381c4
Isabel Llanes <misabellanes@gmail.com>
2024-10-16 15:07:12 +0200

    Add some TODOs

--- PowerTrades.Application/inbound/GeneratePowerTradeForecastReportUseCase.cs
+++ PowerTrades.Application/inbound/GeneratePowerTradeForecastReportUseCase.cs
@@ -8,16 +8,18 @@ namespace PowerTrades.Application.inbound
         private const int HOURS_IN_DAY = 24;
 
         public PowerTradeForecastReport Generate() {
-            List<PowerTrade> powerTrades = powerTradeRepository.GetPowerTrades(DateTime.Now);
+            List<PowerTrade> powerTrades = powerTradeRepository.GetPowerTrades(DateTime.Now); //TODO: Include right date
             var aggregatedPowerTrade = powerTrades.Aggregate((a, b) => a + b);
 
             return new PowerTradeForecastReport
             {
                 Periods = Enumerable.Range(1, HOURS_IN_DAY)
                             .Select(hourOfTheDay => new ForecastedPowerPeriod {
-                                DateTime = DateTime.Now,
+                                DateTime = DateTime.Now, //TODO: Include right date
                                 AggregatedVolume = aggregatedPowerTrade.GetPeriod(hourOfTheDay).Volume })
                             .ToList()
+                //TODO:ForecastedDay
+                //TODO: ExecutionTimestamp
             };
            }
     }
