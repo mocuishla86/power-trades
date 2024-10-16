095a5701027f8e13249b373085d9d80a83b43912
Isabel Llanes <misabellanes@gmail.com>
2024-10-16 11:20:53 +0200

    Create Application Layer

---
+++ PowerTrades.Application/inbound/GeneratePowerTradeForecastReportUseCase.cs
@@ -0,0 +1,10 @@
+﻿using PowerTrades.Application.outbound;
+using PowerTrades.domain;
+
+namespace PowerTrades.Application.inbound
+{
+    public class GeneratePowerTradeForecastReportUseCase(IPowerTradeRepository powerTradeRepository)
+    {
+        public PowerTradeForecastReport Generate() { return null; }
+    }
+}
---
+++ PowerTrades.Application/outbound/IPowerTradeRepository.cs
@@ -0,0 +1,9 @@
+﻿using PowerTrades.domain;
+
+namespace PowerTrades.Application.outbound
+{
+    public interface IPowerTradeRepository
+    {
+        List<PowerTrade> GetPowerTrades(DateTime date);
+    }
+}
